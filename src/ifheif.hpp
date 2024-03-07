7f907cbd36fc4dfc9f3d4e1a21f124ff384d1ef4
Mr-Ojii <okaschan@gmail.com>
2024-03-07 20:20:03 +0900

    plugin_infoについて、コメントを追記

--- src/ifheif.hpp
+++ src/ifheif.hpp
@@ -3,6 +3,10 @@
 #include <cstdint>
 #include <windows.h>
 
+// https://www2f.biglobe.ne.jp/~kana/spi_api/spi_getplugininfo.html
+// 規格の2n+2, 2n+3について、AviUtlの拡張編集に読み込ませるために複数ファイル形式を1つにまとめた書き方をする。
+// Susie-Pluginの2n+2に対しては問題が発生しない実装にしたため、規格に準拠しているホストであれば、正常に読み込みが可能であると思われる。
+// https://scrapbox.io/Mr-Ojii/AviUtl%E3%81%AE%E6%8B%A1%E5%BC%B5%E7%B7%A8%E9%9B%86%E3%81%ABSusie%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3%E3%82%92%E5%B0%8E%E5%85%A5%E3%81%97%E3%81%9F%E3%81%8C%E3%80%81%E7%94%BB%E5%83%8F%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%82%92%E8%AA%AD%E3%81%BF%E8%BE%BC%E3%81%BE%E3%81%AA%E3%81%84
 static const char* plugin_info[] = {
     "00IN",
     "HEIF AVIF Reader by Mr-Ojii",
